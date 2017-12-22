576978498fe21d64422fc054b0ac513a3e2ecd12
justin12140 <34732057+justin12140@users.noreply.github.com>
2017-12-22 18:49:16 -0500

    Update coin_estimator_by_weight.py

--- coin_estimator_by_weight.py
+++ coin_estimator_by_weight.py
@@ -3,54 +3,57 @@
 
 import math
 
-print('This program will take the weight of the coins you have, and tell you how many wrappers you can fit.')
-print('Would you like to submit weight in pounds or grams?')
+grams_dict = {
+    'penny' : 2.5,
+    'nickle': 5,
+    'dime': 2.268,
+    'quarter' : 5.67,
+    'penny_dime_limit' : 50,
+    'nickle_quarter_limit' : 40 
+                    }
+    
+    
+def pound_converstion(unit):
+    converted_unit = unit * 0.00220462
+    return converted_unit
+    
+    
+def clean_input():
+    user_input = int(input('>').strip())
+    return user_input
 
-x = 0
-coins = ['pennies', 'nickles','dimes', 'quarters']
-pound_conversion = 0.00220462
-pennies_grams = 2.5
-pennies_pounds = pennies_grams * pound_conversion
-pennies_limit = 50
-nickles_grams = 5
-nickles_pounds = nickles_grams * pound_conversion
-nickles_limit = 40
-dimes_grams = 2.268
-dimes_pounds = dimes_grams * pound_conversion
-dimes_limit = 50
-quarters_grams = 5.67
-quarters_pounds = quarters_grams * pound_conversion
-quarters_limit = 40
 
-final_weight = 'Unresolved'
 def coin_check(coin_type, coin_weight, coin_limit):
-    global final_weight
-    final_weight = 'Unresolved'
-    while final_weight == 'Unresolved':
+    while True:
         print('What is the weight of the ' + coin_type.title() + ' you have?')
-        user_input = int(input('> '))
-        if str(user_input).strip().isdigit() == True and user_input != 0:
-            total_wrapped = math.ceil(((user_input / coin_weight)/coin_limit))
-            print("You have a total of " + str(int(user_input//coin_weight)) + ' ' + coin_type.title() +
-                  " which can be placed in %d wrappings!" %total_wrapped)
-            final_weight = "Resolved"
+        clean_input()
+        if clean_input() > 0:
+            total_wrapped = math.ceil(((clean_input() / coin_weight) / coin_limit))
+            print("You have a total of ", int(clean_input() // coin_weight),' ', coin_type.title(),
+                  " which can be placed in %d wrappings!" % total_wrapped)
+            break
+
         else:
             print("That is not a valid value! Please try again.")
 
-while x == 0:
+            
+print('This program will take the weight of the coins you have, and tell you how many wrappers you can fit.')
+print('Would you like to submit weight in pounds or grams?')
+
+while True:
     weight_type = input('> ')
     if weight_type.strip() == 'pounds' or weight_type.strip() == 'Pounds':
-        coin_check('pennies',pennies_pounds,pennies_limit)
-        coin_check('nickles', nickles_pounds,nickles_limit)
-        coin_check('dimes', dimes_pounds,dimes_limit)
-        coin_check('quarters',quarters_pounds, quarters_limit)
-        x = 1
+        coin_check('pennies', pound_converstion( grams_dict['penny']), grams_dict['penny_dime_limit'])
+        coin_check('nickles', pound_converstion( grams_dict['nickle']),grams_dict['nickle_quarter_limit'])
+        coin_check('dimes', pound_converstion( grams_dict['dime']),grams_dict['penny_dime_limit'])
+        coin_check('quarters',pound_converstion( grams_dict['quarter']), grams_dict['nickle_quarter_limit'])
+        break
     elif weight_type.strip() == 'grams' or weight_type.strip() == 'Grams':
-        coin_check('pennies', pennies_grams, pennies_limit)
-        coin_check('nickles', nickles_grams, nickles_limit)
-        coin_check('dimes', dimes_grams, dimes_limit)
-        coin_check('quarters', quarters_grams, quarters_limit)
-        x = 1
+        coin_check('pennies', grams_dict['penny'], grams_dict['penny_dime_limit'])
+        coin_check('nickles', grams_dict['nickle'], grams_dict['nickle_quarter_limit'])
+        coin_check('dimes', grams_dict['dime'], grams_dict['penny_dime_limit'])
+        coin_check('quarters', grams_dict['quarter'], grams_dict['nickle_quarter_limit'])
+        break
     else:
         print("This is not a valid entry. Please try again.")
 
